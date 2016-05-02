6bb20d3169324dc89051d59da01c6c5aaef9efce
martinerko <martinerko@gmail.com>
2016-05-02 19:39:31 +1000

    Query statistics module

---
+++ index.js
@@ -0,0 +1,11 @@
+/*!
+ * mongo-log-analyzer
+ * Copyright(c) 2016 martinerko <martinerko@gmail.com>
+ * MIT Licensed
+ */
+
+module.exports = {
+  miner: require('./lib/miner'),
+  parser: require('./lib/parser'),
+  stats: require('./lib/stats')
+};
